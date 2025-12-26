2d549367e7b591ac4c51bb0dab2771ca7f0e2a42
lattalab <91266449+lattalab@users.noreply.github.com>
2025-12-27 02:09:59 +0800

    Time: 10 ms (46.65%) | Memory: 11.4 MB (21.00%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast){
+                // meet
+                // we have such formula: x = (n - 1) (y + z) + z
+                ListNode *index1 = fast;
+                ListNode *index2 = head;
+                while(index1 != index2){
+                    index1 = index1->next;
+                    index2 = index2->next;
+                }
+                return index2;
+            }
+        }
+        // no ring
+        return NULL;
+    }
+};
