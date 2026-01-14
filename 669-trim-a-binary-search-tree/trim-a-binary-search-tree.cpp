8ceb1b1fecd16a9400012e505e0f76597c7cbd84
lattalab <91266449+lattalab@users.noreply.github.com>
2026-01-15 04:15:08 +0800

    Time: 0 ms (100.00%) | Memory: 24.8 MB (55.51%) - LeetSync

---
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if (root == NULL) return NULL;
+
+        if (root->val > high) {
+            TreeNode *left = trimBST(root->left, low, high);
+            return left;
+        }
+
+        if (root->val < low) {
+            TreeNode *right = trimBST(root->right, low, high);
+            return right;
+        }
+
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        return root;
+    }
+};
