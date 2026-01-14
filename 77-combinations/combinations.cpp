ec80c851cd729604ff3cec9b0843c341b1713c05
lattalab <91266449+lattalab@users.noreply.github.com>
2026-01-15 04:14:10 +0800

    Time: 73 ms (35.18%) | Memory: 82.4 MB (40.69%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking (int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+
+        for (int i=startIndex; i<=n; i++) {
+            path.push_back(i);
+            backtracking(n, k, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        // clear the containers
+        path.clear();
+        result.clear();
+        backtracking(n, k, 1);
+        return result;
+    }
+};
