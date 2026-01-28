6a4466713a99772bc25589f2a8a3cee318e26b9a
lattalab <91266449+lattalab@users.noreply.github.com>
2026-01-28 21:25:48 +0800

    Time: 0 ms (100.00%) | Memory: 40.1 MB (79.51%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(), 1);
+        int left = 1;
+        for (int i=0; i<nums.size(); i++){
+            ans[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;        
+        for (int j=nums.size()-1; j>=0; j--) {
+            ans[j] *= right;
+            right *= nums[j];
+        }
+        return ans;
+    }
+};
