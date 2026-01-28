a4660ef874ee194d929b4b09045d7e29421655be
lattalab <91266449+lattalab@users.noreply.github.com>
2026-01-28 21:55:44 +0800

    Time: 7 ms (29.04%) | Memory: 25.8 MB (13.50%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> rows[9], cols[9], boxes[9];
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                if (board[i][j] == '.') continue;
+
+                int boxIdx = 3 * (i/3) + (j/3);
+
+                // 有重複出現的話
+                if (rows[i].count(board[i][j]) || cols[j].count(board[i][j]) || boxes[boxIdx].count(board[i][j])) {
+                    return false;
+                }
+
+                rows[i].insert(board[i][j]);
+                cols[j].insert(board[i][j]);
+                boxes[boxIdx].insert(board[i][j]);
+            }
+        }
+
+        return true;
+    }
+};
