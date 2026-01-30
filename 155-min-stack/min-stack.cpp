257a7c18ef779211bbc276ce26f0da1726ae5f73
lattalab <91266449+lattalab@users.noreply.github.com>
2026-01-30 23:27:59 +0800

    Time: 3 ms (61.66%) | Memory: 24 MB (15.17%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+public:
+    vector<vector<int>> stack;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        int min = getMin();
+        if (stack.empty() || min > val) min = val;
+        stack.push_back({val, min});
+    }
+    
+    void pop() {
+        stack.pop_back();
+    }
+    
+    int top() {
+        if (stack.empty()) return -1;
+        return stack.back()[0];
+    }
+    
+    int getMin() {
+        if (stack.empty()) return -1;
+        return stack.back()[1];
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
