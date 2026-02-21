bbb18c2f8b6f9d4028c77ee3f017b49964f18521
lattalab <91266449+lattalab@users.noreply.github.com>
2026-02-21 16:09:09 +0800

    Time: 7 ms (30.94%) | Memory: 19.3 MB (69.50%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,5 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0: return 0
-        if x == 1: return 1
-        return int(x**0.5)
+        left = 1
+        right = x
+
+        # python didn;t have overflow issue
+        while left <= right:
+            mid = (left + right) // 2
+            if (mid * mid > x):
+                right = mid -1
+            elif (mid * mid < x):
+                left = mid + 1
+            else:
+                return mid
+    
+        return left - 1
