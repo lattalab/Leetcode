3204b38e2f0e9838a21ecc03807d856a9ee370e4
lattalab <91266449+lattalab@users.noreply.github.com>
2026-02-23 14:43:57 +0800

    Time: 0 ms (100.00%) | Memory: 22.9 MB (54.29%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        // 1. find the middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        // 2. reverse the other half part
+        // 4->5 to 5->4
+        ListNode* second = slow->next;
+        slow->next = NULL;
+        ListNode* prev = NULL;
+        while (second != NULL) {
+            ListNode* temp = second->next;
+            second->next = prev;
+            prev = second;
+            second = temp;
+        }
+        // 3. merge the two lists
+        ListNode* first = head;
+        second = prev;
+        while (second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
