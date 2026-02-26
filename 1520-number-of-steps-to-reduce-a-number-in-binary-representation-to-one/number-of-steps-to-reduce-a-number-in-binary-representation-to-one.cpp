705c2a295b9b215c55603aacee148bda1a24a4ec
lattalab <91266449+lattalab@users.noreply.github.com>
2026-02-26 21:25:35 +0800

    Time: 0 ms (100.00%) | Memory: 8.9 MB (58.55%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSteps(string s) {
+        int ans = 0;
+        int carry = 0;
+        for (int i=s.size()-1; i>0; i--) {
+            int bit = (int) s[i] - '0';
+            bit += carry;
+
+            if (bit == 1) {
+                ans += 2;
+                carry = 1;
+            } else {
+                ans += 1;
+            }
+        }
+        return ans + carry;
+    }
+};
