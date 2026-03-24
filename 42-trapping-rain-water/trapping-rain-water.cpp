c0b885c18a57e1a1f9d35cf89af53bafe3c0e221
lattalab <91266449+lattalab@users.noreply.github.com>
2026-03-25 01:54:57 +0800

    Time: 0 ms (100.00%) | Memory: 27.1 MB (34.75%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int ans = 0;
-        int left =0, right = height.size() - 1;
-        int maxLeft= height[left], maxRight = height[right];
-        while (left < right) {
-            if (height[left] < height[right]) {
-                left++;
-                maxLeft = max(maxLeft, height[left]);
-                ans += (maxLeft - height[left]);
+        int ans = 0 ;
+
+        stack<int> st;
+        st.push(0);
+        for (int i=1; i<height.size(); i++) {
+            if (height[i] <= height[st.top()]) {
+                st.push(i);
             }
             else {
-                right--;
-                maxRight = max(maxRight, height[right]);
-                ans += (maxRight - height[right]);
+                while (!st.empty() && height[i] > height[st.top()]) {
+                    // find the rightmost first bigger value
+                    int mid = height[st.top()];
+                    st.pop();
+                    if (!st.empty()) {
+                        int w = i - st.top() - 1;
+                        // find minimum value of left and right side height
+                        int h = min(height[st.top()], height[i]) - mid;
+                        ans += (h * w);
+                    }
+                }
+                st.push(i);
             }
         }
         return ans;
