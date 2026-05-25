f6a7134febade9899d4d654d76fc992d8a8369dc
lattalab <91266449+lattalab@users.noreply.github.com>
2026-05-25 19:22:11 +0800

    Time: 0 ms (100.00%) | Memory: 27.1 MB (38.42%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -12,24 +12,18 @@ public:
         if (nums.size() != ExpectedLen) return false;
 
         // trace for number occurance
-        vector<bool> test(ExpectedLen, false);
-        int count = 1;
+        vector<int> count(ExpectedLen, 0);
         for (int i=0; i<nums.size(); i++) {
-            int target = nums[i];
-            if (test[target] == false) {
-                test[target] = true;
+            int curr = nums[i];
+            // check if [1, n-1] appear more than once
+            if (curr <= max-1 && count[curr] > 0) {
+                return false;
             }
-            else {
-                if (target != max) {
-                    return false;
-                }
-                else {
-                    count++;
-                    if (count != 2) {
-                        return false;
-                    }
-                }
+            // check if n appear at most twice
+            if (curr == max && count[curr] > 1) {
+                return false;
             }
+            count[curr]++;
         }
         return true;
     }
