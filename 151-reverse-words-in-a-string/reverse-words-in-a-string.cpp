509034688f89d85f606c53de047c20236874e1d2
lattalab <91266449+lattalab@users.noreply.github.com>
2026-05-31 21:42:03 +0800

    Time: 0 ms (100.00%) | Memory: 9.9 MB (87.22%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    void removeExtraSpaces (string &s){
-        int slow = 0, fast = 0;
-        for (fast=0; fast<s.size(); fast++){
-            if (s[fast] != ' '){
-                // pad an space between words
+    void RemoveExtraSpace (string &s) {
+        int slow =0;
+        for (int fast = 0; fast < s.size(); fast++) {
+            // remove preceding space
+            if (s[fast] != ' ') {
+                // intermdeia space
                 if (slow != 0) s[slow++] = ' ';
-                // copy entire word
-                while (fast < s.size() && s[fast] != ' ')
+                while (fast < s.size() && s[fast] != ' ') {
                     s[slow++] = s[fast++];
+                }
             }
         }
         s.resize(slow);
     }
-    void reverse(string& s, int start, int end){
-        for (int i=start, j=end; i<j; i++, j--){
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
+    void reverseRange(string &s, int left, int right) {
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
     }
     string reverseWords(string s) {
-        removeExtraSpaces(s); // remove redundent space
-        reverse(s, 0, s.size()-1); // reverse entire strings
-        int start = 0;
-        for(int i=0; i<=s.size(); i++){
-            // because the last word need to do reverse by s.size()
-            if (s[i] == ' ' || i==s.size()){
-                int end = i-1;
-                reverse(s, start, end);
-                start = i+1;
+        // 1. remove extra space
+        RemoveExtraSpace(s);
+        // 2. reverse entire string
+        reverse(s.begin(), s.end());
+        // 3. reverse the word
+        int left = 0;
+        for (int i=0; i<=s.size(); i++) {
+            if (i == s.size() || s[i] == ' ') {
+                reverseRange(s, left, i-1);
+                left = i+1;
             }
         }
         return s;
