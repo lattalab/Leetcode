a868801652d503099bf506c117d5048e18d7e4cf
lattalab <91266449+lattalab@users.noreply.github.com>
2026-06-29 03:24:15 +0800

    Time: 14 ms (14.69%) | Memory: 55.2 MB (69.21%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int ans = 1;
+        arr[0] = 1;     // assume decrease to 1
+        for (int i=1; i<arr.size(); i++) {
+            int diff = abs(arr[i]-arr[i-1]);
+            if (diff > 1) {
+                arr[i] = arr[i-1] + 1;
+            }
+            ans = max(ans, arr[i]);
+        }
+        return ans;
+    }
+};
