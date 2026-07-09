a49488f00297cc680e7bdd50a8ae99c2fe766c88
lattalab <91266449+lattalab@users.noreply.github.com>
2026-07-09 19:17:27 +0800

    Time: 0 ms (100.00%) | Memory: 12.4 MB (68.04%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        
+        ans = ""
+        # select strs[0], traverse at each character
+        for i in range(len(strs[0])):
+            ch = strs[0][i]
+            # vertical finding
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != ch:
+                    return ans
+            
+            ans += ch
+        
+        return ans
