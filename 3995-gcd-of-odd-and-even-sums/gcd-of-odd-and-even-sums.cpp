06b005dcf8f9a50354bfde8fbe0fda746206988b
lattalab <91266449+lattalab@users.noreply.github.com>
2026-07-15 22:33:54 +0800

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.02%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // odd sequence = n * n
+        // even sequence = (n+1) * n
+        // so the gcd is n
+        return n;
+    }
+};
