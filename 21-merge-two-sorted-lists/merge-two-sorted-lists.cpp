9272cc930a330fb7449de65eec629af97b2bee4c
lattalab <91266449+lattalab@users.noreply.github.com>
2026-07-25 03:00:14 +0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (88.04%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,17 +11,27 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (!list1 && list2 ) return list2;
-        if ( list1 && !list2 ) return list1;
-        if (!list1 && !list2) return NULL;
-        
-        if (list1->val > list2->val) {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+        ListNode *dummy = new ListNode();
+        ListNode *cur = dummy;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                cur->next= list1;
+                cur = cur->next;
+                list1 = list1->next;
+            }
+            else {
+                cur->next = list2;
+                cur = cur->next;
+                list2 = list2->next;
+            }
         }
-        else {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        }
+        // remaining parts
+        if (list1) cur->next = list1;
+        if (list2) cur->next = list2;
+
+        ListNode *head = dummy->next;
+        delete dummy;
+        return head;
     }
 };
