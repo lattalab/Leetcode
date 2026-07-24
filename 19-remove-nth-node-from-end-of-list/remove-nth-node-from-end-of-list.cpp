4ceb54e9ab774ea33a8cdf017c6927f0f16eaa66
lattalab <91266449+lattalab@users.noreply.github.com>
2026-07-25 02:36:34 +0800

    Time: 0 ms (100.00%) | Memory: 14.9 MB (93.69%) - LeetSync

---
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *dummy = new ListNode();
+        dummy->next = head;
+
+        ListNode *slow = dummy;
+        ListNode *fast = dummy;
+        // cause add extra dummy head, need to add 1 gap
+        for (int i=0; i<n+1; i++) {
+            fast = fast->next;
+        }
+
+        while (fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // delete
+        ListNode *temp = slow->next;
+        slow->next = temp->next;
+        delete temp;
+        // avoid null pointer access
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
