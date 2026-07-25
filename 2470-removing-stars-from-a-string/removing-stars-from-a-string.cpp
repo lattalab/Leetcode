9b98e94d70e13dec9cf8caa3c440d00c679b7772
lattalab <91266449+lattalab@users.noreply.github.com>
2026-07-25 23:57:00 +0800

    Time: 21 ms (41.93%) | Memory: 29.9 MB (24.21%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+        for (int i=0; i<s.size(); i++) {
+            if (s[i] == '*') {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            }
+            else {
+                st.push(s[i]);
+            }
+        }
+
+        // revert to string
+        string ans;
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+
+    }
+};
