a73191de3c1e75d87f6e71dbbfa1e7f9f66f957e
lattalab <91266449+lattalab@users.noreply.github.com>
2026-08-03 12:57:36 +0800

    Time: 356 ms (75.64%) | Memory: 14.6 MB (62.14%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def networkDelayTime(self, times, n, k):
+        """
+        :type times: List[List[int]]
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        # build adjacency list
+        graph = [[] for _ in range(n+1)]
+        for s, t, v in times:
+            graph[s].append((t, v))
+
+        # create distance array
+        INT_MAX = 1e9
+        minDist = [INT_MAX] * (n+1)
+
+        # queue optimized bellmen ford
+        q = deque()
+        q.append(k)
+        minDist[k] = 0
+
+        while q:    # !q.empty()
+            src = q.popleft()
+
+            # relax all outgoing edges
+            for t, v in graph[src]:
+                # update the neighbor
+                if minDist[src] + v < minDist[t]:
+                    minDist[t] = minDist[src] + v
+                    q.append(t)
+
+        ans = 0
+        for i in range(1, n+1):
+           
+            if minDist[i] == INT_MAX:
+                return -1
+
+            ans = max(ans, minDist[i])
+
+        return ans
