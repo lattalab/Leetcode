41c915e6a25b9b5d1dcf59f19f47f152764f2075
lattalab <91266449+lattalab@users.noreply.github.com>
2026-08-17 18:11:35 +0800

    Time: 35 ms (83.34%) | Memory: 76.8 MB (76.53%) - LeetSync

---
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        priority_queue<int, vector<int>, greater<int>> left, right;
+        int n = costs.size();
+        // maintain 2 heap array,
+        // index i and j means valid successor for next elment to push
+        for (int i=0; i<candidates; i++) left.push(costs[i]);
+        for (int j=max(candidates, n-candidates); j<n; j++) right.push(costs[j]);
+
+        // compare 2 element
+        long long ans = 0;
+        int nextHead = candidates;
+        int nextTail = n - 1 - candidates;
+        for (int i=0; i<k; i++) {
+            if ( right.empty() || (!left.empty() && !right.empty() && left.top() <= right.top()) ) {
+                ans += left.top();
+                left.pop();
+
+                // refill when there exist worker
+                if (nextHead <= nextTail) {
+                    left.push(costs[nextHead]);
+                    nextHead++;
+                }
+            }
+            else {
+                ans += right.top();
+                right.pop();
+
+                // refill when there exist worker
+                if (nextHead <= nextTail) {
+                    right.push(costs[nextTail]);
+                    nextTail--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
