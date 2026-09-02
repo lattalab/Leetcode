5cca399af9478d9af61d8a263107b4bd7b55a849
lattalab <91266449+lattalab@users.noreply.github.com>
2026-09-02 21:44:55 +0800

    Time: 0 ms (100.00%) | Memory: 30.4 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // 3 cases: 
+        // 1 odd 1 even: odd
+        // 1 odd 1 odd : even
+        // 1 even 1 even: even
+        return true;
+    }
+};
