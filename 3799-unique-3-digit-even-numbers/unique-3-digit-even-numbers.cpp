dfab0e132ded3b275fbc602b161995e53493f1f9
lattalab <91266449+lattalab@users.noreply.github.com>
2026-09-11 11:21:15 +0800

    Time: 10 ms (46.94%) | Memory: 30.9 MB (66.84%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int n = digits.size();
+        int ans = 0;
+        unordered_set<int> st;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                for (int k=0; k<n; k++) {
+                    if (j != i && k != j && k != i) {
+                        int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                        // even number
+                        if (num >= 100 && num % 2 == 0) {
+                            st.insert(num);
+                        }
+                    }
+                }
+            }
+        }
+        return st.size();
+    }
+};
